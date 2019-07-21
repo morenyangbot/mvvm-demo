1b62ce2e21836c6e1bf3586a027bd65113bc7dc7
Teng YANG <morenyang88@gmail.com>
2019-07-21 18:26:09 +0800

    createReactive

---
+++ docs/examples/example2.js
@@ -0,0 +1,29 @@
+/* eslint-disable */
+function createReactive(obj, key, value) {
+  Object.defineProperty(obj, key, {
+    enumerable: true,
+    configurable: true,
+    get() {
+      return value;
+    },
+    set(newVal) {
+      // 更新绑定数据
+      console.log(`update:${key}`, value, newVal);
+      value = newVal;
+    }
+  });
+}
+
+const obj = {
+  name: 'Caixukun',
+  hobby: 'Sing, Jump, Rap, Basketbal'
+};
+
+// 遍历obj中的各个元素，并将他们添加到Reactive中
+Object.keys(obj).forEach(key => {
+  createReactive(obj, key, obj[key]);
+});
+
+// 尝试修改obj中元素的值
+
+obj.name = 'Kunkun'
--- docs/readme.md
+++ docs/readme.md
@@ -1,6 +1,8 @@
 # Vue Sharing
 
-## `Object.defineProperty`
+## Reactive 基础
+
+### `Object.defineProperty`
 
 `Object.defineProperty()` 方法会直接在一个对象上定义一个新属性，或者修改一个对象的现有属性， 并返回这个对象。
 
@@ -8,5 +10,8 @@
 
 > [examples/example1.js](examples/example1.js)
 
-## 数据劫持
+### 数据劫持
 
+> [examples/example2.js](examples/example2.js)
+
+## 追踪依赖
